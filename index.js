c84669991c7d9a9a50da85a3be422a78ebc671de
infolanze807 <techteam.infolanze@gmail.com>
2024-07-07 17:37:24 +0530

    add vercel.json

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://gno-f-rontend.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 // MongoDB connection
 const mongoURI = 'mongodb+srv://kmbmevada2343:pro2wnEJusERya71@gnosiswallet.oq1wvwv.mongodb.net/Users';
